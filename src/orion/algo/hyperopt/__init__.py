211ac5c1b4d99346ae4db85175294e35ca84906f
Mike Pieper <mpieper636@gmail.com>
2019-02-22 12:55:09 -0500

    first commit

---
+++ src/orion/algo/hyperopt/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+:mod:`orion.algo.hyperopt` -- Wrap up optimization algorithm from Hyperopt
+==============================================================================
+.. module:: hyperopt
+   :platform: Unix
+   :synopsis: Expose subclasses of `orion.algo.base.BaseAlgorithm` that use hyperopt
+"""
